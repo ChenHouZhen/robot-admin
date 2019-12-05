1be157e5cfce38db7e1095ffb9821db8a42a01d4
chenhouzhen <chenhouzhen@hqjy.com>
2019-12-05 11:47:29 +0800

    k

---
+++ robot-admin-ui/src/components/Knowledge.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>{{msg}}</h1>
+    </div>
+</template>
+
+
+
+<<script>
+export default {
+    name: 'Knowledge',
+    data() {
+        return {
+            msg:"知识点管理页面"
+        }
+    },
+}
+</script>
+
+
