dd34dc1bfa6aaecc72f6bb9864338e1ab0b4b883
chenhouzhen <chenhouzhen@hqjy.com>
2019-12-10 18:11:28 +0800

    server

--- robot-admin-server/src/main/java/com/chenhz/server/controller/SysMenuController.java
+++ robot-admin-server/src/main/java/com/chenhz/server/controller/SysMenuController.java
@@ -1,6 +1,11 @@
 package com.chenhz.server.controller;
 
 
+import com.chenhz.server.service.SysMenuService;
+import com.chenhz.server.utils.R;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +19,21 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2019-12-10
  */
 @RestController
-@RequestMapping("/sys-menu")
+@RequestMapping("/menu")
 public class SysMenuController {
 
+    @Autowired
+    private SysMenuService sysMenuService;
+
+    @GetMapping(value = "/list")
+    @ApiOperation("获取全部菜单")
+    public R list(){
+        return R.ok().put("data",sysMenuService.list());
+    }
+
+    @GetMapping(value = "/get")
+    @ApiOperation("获取全部菜单")
+    public R get(Long id){
+        return R.ok().put("dat",sysMenuService.getById(id));
+    }
 }
