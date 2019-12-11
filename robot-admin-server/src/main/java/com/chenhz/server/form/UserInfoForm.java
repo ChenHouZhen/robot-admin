742b7c7384d7c7f6ab985a282db16956043b53ee
chenhouzhen <chenhouzhen@hqjy.com>
2019-12-11 12:06:14 +0800

    data

--- robot-admin-server/src/main/java/com/chenhz/server/form/UserInfoForm.java
+++ robot-admin-server/src/main/java/com/chenhz/server/form/UserInfoForm.java
@@ -1,8 +1,10 @@
 package com.chenhz.server.form;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
@@ -31,8 +33,18 @@ public class UserInfoForm {
     @ApiModelProperty(value = "部门ID",example = "1")
     private Integer deptId;
 
+    /**
+     * Springboot使用的默认json解析框架是jackjson框架
+     *
+     * jackjson解析框架在解析实体类里面是date数据类型的数据时的默认格式是：UTC类型
+     *  即yyyy-MM-dd'T'HH:mm:ss.SSS 并且默认为+8时区，即时间基础上加8小时
+     *
+     *  参考：https://my.oschina.net/zicheng/blog/2963117
+     */
     @ApiModelProperty(value = "开始时间")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date startTime;
+
 
     @ApiModelProperty(value = "结束时间")
     private Date endTime;
